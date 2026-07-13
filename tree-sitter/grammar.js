46389c60764011e19e0f20510f67992730e31458
Xqhare <xqhare@web.de>
2026-07-14 00:54:43 +0200

    fix(tree-sitter): make grammar line-oriented and fix tag tokenization

--- tree-sitter/grammar.js
+++ tree-sitter/grammar.js
@@ -1,34 +1,51 @@
 module.exports = grammar({
 	name: 'nomos',
 
+	extras: $ => [/[ \t\f\v]/],
+
+	conflicts: $ => [
+		[$.version, $.ignored_line],
+		[$.task, $.ignored_line],
+		[$.comment, $.ignored_line],
+	],
+
 	rules: {
-		source_file: $ => seq(optional($.version), repeat1($._item)),
-		_item: $ => choice(
+		source_file: $ => repeat(choice(
+			$.version,
 			$.task,
-			$.comment
-		),
+			$.comment,
+			$.ignored_line,
+			$.empty_line,
+		)),
 
 		// future proof for all versions (0-inf)
-		version: $ => /<!--[\s]+nomos:[\s]+[0-9]+[\s]+-->/,
-		task: $ => seq(
-      optional(/[\s]+/),
+		version: $ => prec.dynamic(2, seq(
+			/<!--[\s]+nomos:[\s]+[0-9]+[\s]+-->/,
+			$._eol
+		)),
+
+		task: $ => prec.dynamic(2, seq(
 			'-',
 			$.stat,
 			optional($.prio),
 			$.title,
-			optional($.delimiter),
-			optional($.dates),
-			optional($.description),
-		),
-		comment: $ => seq(
-      optional(/[\s]+/),
-			'*',
-			$.description,
-		),
+			optional(seq(
+				$.delimiter,
+				optional($.dates),
+				optional($.description)
+			)),
+			$._eol
+		)),
 
-		delimiter: $ => /[\s]+::[\s]+/,
-		stat: $ => seq(' ', '[', $.stat_char , ']', ' '),
-		stat_char: $ => choice(
+		comment: $ => prec.dynamic(2, seq(
+			'*',
+			optional($.description),
+			$._eol
+		)),
+
+		delimiter: $ => '::',
+
+		stat: $ => token(prec(10, seq('[', choice(
 			'b',
 			'B',
 			'c',
@@ -39,35 +56,60 @@ module.exports = grammar({
 			'X',
 			' ',
 			'/',
-		),
-		prio: $ => seq('(', /[0-9]/, ')'),
-		title: $ => /([^:]|:[^:])+/,
-		description: $ => seq(
-			choice(
-				$.text,
-				$.tag
-			),
-			repeat(seq(' ', choice($.text, $.tag)))
-		),
-		text: $ => /[^*]+/,
-		tag: $ => choice(
+		), ']'))),
+
+		prio: $ => seq('(', /[0-9a-zA-Z]/, ')'),
+
+		title: $ => token(prec(1, /([^:\r\n]|:[^:\r\n])+/)),
+
+		description: $ => repeat1(choice(
 			$.kind_tag,
 			$.location_tag,
-			$.kv_tag,
 			$.generic_tag,
-			$.dependency_tag
+			$.dependency_tag,
+			$.kv_tag,
+			$._text_word
+		)),
+
+		kind_tag: $ => token(prec(2, /\+[^\s]+/)),
+		location_tag: $ => token(prec(2, /@[^\s]+/)),
+		generic_tag: $ => token(prec(2, /#[^\s]+/)),
+		dependency_tag: $ => token(prec(3, choice(
+			/dep="[^"\r\n]*"/,
+			/dep=[^\s:]+:"[^"\r\n]*"/,
+			/dep=[^\s]+/
+		))),
+		kv_tag: $ => token(prec(2, choice(
+			/[^\s=]+=[^\s"]+/,
+			/[^\s=]+="[^"\r\n]*"/
+		))),
+		_text_word: $ => token(prec(1, /[^\s]+/)),
+
+		dates: $ => seq(
+			choice(
+				$.date,
+				seq($.date, $.date)
+			)
 		),
-		kind_tag: $ => seq('+', /[^\s]+/),
-		location_tag: $ => seq('@', /[^\s]+/),
-		kv_tag: $ => choice(
-			seq(/[^\s]+/, '=', /[^\s"]+/),
-			seq(/[^\s]+/, '=', '"', /[^"]+/, '"')),
-		generic_tag: $ => seq('#', /[^\s]+/),
-		dependency_tag: $ => choice(
-			seq('dep', '=', '"', /[^"]+/, '"',),
-			seq('dep', '=', /[^\s]+/, ':', '"', /[^"]+/, '"',),			
-		),
-		dates: $ => seq(choice($.date, seq($.date, ' ', $.date)), ' '),
+
 		date: $ => /\d{4}-\d{2}-\d{2}/,
+
+		ignored_line: $ => prec.dynamic(1, seq(
+			repeat1(choice(
+				/[^\r\n\s\[\]\(\)\-\*:]+/,
+				'[',
+				']',
+				'-',
+				'*',
+				'(',
+				')',
+				':',
+			)),
+			$._eol
+		)),
+
+		empty_line: $ => $._eol,
+
+		_eol: $ => choice('\n', '\r\n'),
 	}
 })
